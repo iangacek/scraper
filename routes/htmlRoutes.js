5f137531777d02bd33ecfa9957fcab312ae8436d
Ian Gacek <gacek004@umn.edu>
2019-08-15 12:48:46 -0500

    JS additions

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,46 @@
+var db = require("../models");
+
+module.exports = function(app) {
+  // Load index page
+  app.get("/", function(req, res) {
+    db.Names.findAll({}).then(function(dbExamples) {
+      res.render("index", {
+      });
+    });
+
+    app.get("/faq", function(req, res) {
+      res.render("faq");
+    });
+
+    app.get("/createchore", function(req, res) {
+      res.render("createChore");
+    });
+
+    // Load example page and pass in an example by id
+    app.get("/chores/:id", function(req, res) {
+      db.Name.findOne({ where: { id: req.params.id } }).then(function(
+        dbExample
+      ) {
+        res.render("example", {
+          example: dbExample
+        });
+      });
+    });
+
+    // Load example page and pass in an example by id
+    app.get("/chores/:id", function(req, res) {
+      db.Chore.findOne({ where: { id: req.params.id } }).then(function(
+        dbExample
+      ) {
+        res.render("example", {
+          example: dbExample
+        });
+      });
+    });
+
+    // Render 404 page for any unmatched routes
+    app.get("*", function(req, res) {
+      res.render("404");
+    });
+  });
+};
--- server.js
+++ server.js
@@ -23,9 +23,9 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect(MONGODB_URI);
+// mongoose.connect(MONGODB_URI);
 
 // Routes
 
