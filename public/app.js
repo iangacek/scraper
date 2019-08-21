b38a76ff0da8d93f4aa96bda8047b0bc532aee5a
Ian Gacek <gacek004@umn.edu>
2019-08-20 21:26:52 -0500

    big CSS update

--- public/app.js
+++ public/app.js
@@ -3,13 +3,13 @@ $.getJSON("/articles", function(data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    $("#articles").append("<div class='card-header'><h5 data-id='" + data[i]._id + "'>" + data[i].title + "</h5>" + data[i].link + "</p></div>");
   }
 });
 
 
 // Whenever someone clicks a p tag
-$(document).on("click", "p", function() {
+$(document).on("click", "h5", function() {
   // Empty the notes from the note section
   $("#notes").empty();
   // Save the id from the p tag
