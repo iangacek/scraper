33de850bb0e7e20cb93d59b54d1758fb34692053
Ian Gacek <gacek004@umn.edu>
2019-08-20 22:59:41 -0500

    Fixed notes

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ $.getJSON("/articles", function(data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-    $("#articles").append("<div class='card-header'><h5 data-id='" + data[i]._id + "'>" + data[i].title + "</h5>" + data[i].link + "</p></div>");
+    $("#articles").append("<div class='card-header'><h5 data-id='" + data[i]._id + "'>" + data[i].title + "</h5><p>" + data[i].link + "</p></div>");
   }
 });
 
